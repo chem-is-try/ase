46c8efd76d56932aa539da361d90569b11d6c144
Ask Hjorth Larsen <askhl@proton.me>
2026-04-11 18:22:04 +0200

    apply ruff to test/neighbor, test/spectrum test/transport

--- ase/test/neighbor/test_neighbor_initialization.py
+++ ase/test/neighbor/test_neighbor_initialization.py
@@ -1,4 +1,3 @@
-# fmt: off
 import pytest
 
 from ase.build import bulk
@@ -10,10 +9,10 @@ def test_neighborlist_initialization():
     atoms = bulk('Al', 'fcc', a=4)
 
     nl = NeighborList([8] * len(atoms), skin=0, self_interaction=False)
-    with pytest.raises(Exception, match="Must call update"):
+    with pytest.raises(Exception, match='Must call update'):
         nl.get_neighbors(0)
 
-    with pytest.raises(Exception, match="Must call update"):
+    with pytest.raises(Exception, match='Must call update'):
         nl.get_connectivity_matrix()
 
     nl.update(atoms)
