803e0df9c44a3049e7ffc6da9d305568b435da76
Simiam Andreas Ghan <sangh@dtu.dk>
2026-04-27 15:13:04 +0200

    add comments to script

--- examples/gallery/render_povray_isosurface.py
+++ examples/gallery/render_povray_isosurface.py
@@ -5,28 +5,26 @@ Isosurface with povray
 
 """
 
+# %%
+#Render an isosurface and atoms with povray
+# %%
+
 import numpy as np
 
 from ase.build import bulk
 from ase.io import write
 
-spin_cut_off = 0.4
 density_cut_off = 0.15
 
 rotation = '24x, 34y, 14z'
 
 atoms = bulk('Au', cubic=True) * (2, 2, 2)
 
-# generate dummy volumetric data
 axis = np.linspace(-1, 1, 100)
 X, Y, Z = np.meshgrid(axis, axis, axis)
 isodata = np.exp(-0.5 * (X**2 + 2 * Y**2 + 3 * Z**2))
 
-# Make a gaussian cube file.
-# write('test.cube', atoms)
-
 povray_settings = {
-    # For povray files only
     'pause': False,  # Pause when done rendering (only if display)
     'transparent': False,  # Transparent background
     'canvas_width': None,  # Width of canvas in pixels
@@ -36,28 +34,14 @@ povray_settings = {
     'textures': len(atoms) * ['ase3'],
 }
 
-# some more options:
-# 'image_plane'  : None,  # Distance from front atom to image plane
-#                         # (focal depth for perspective)
-# 'camera_type'  : 'perspective', # perspective, ultra_wide_angle
-# 'point_lights' : [],             # [[loc1, color1], [loc2, color2],...]
-# 'area_light'   : [(2., 3., 40.) ,# location
-#                   'White',       # color
-#                   .7, .7, 3, 3], # width, height, Nlamps_x, Nlamps_y
-# 'background'   : 'White',        # color
-# 'textures'     : tex, # Length of atoms list of texture names
-# 'celllinewidth': 0.05, # Radius of the cylinders representing the cell
-
 generic_projection_settings = {
     'rotation': rotation,
     'radii': atoms.positions.shape[0] * [0.3],
     'show_unit_cell': 1,
 }
 
-# write returns a renderer object which needs to have the render method called
-
 write(
-    'NiO_marching_cubes1.pov',
+    'isosurface.pov',
     atoms,
     **generic_projection_settings,
     povray_settings=povray_settings,
